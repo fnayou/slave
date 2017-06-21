6a8b56a625e5d6fdf91657528e3af16959651c91
Aymen FNAYOU <fnayou.aymen@gmail.com>
2017-06-21 16:34:16 +0200

    Fixes #4

--- src/Slave/Provider/EventDispatcher/EventDispatcherProvider.php
+++ src/Slave/Provider/EventDispatcher/EventDispatcherProvider.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of the Slave package.
+ * This file is part of the fnayou/slave package.
  *
  * Copyright (c) 2016. Aymen FNAYOU <fnayou.aymen@gmail.com>
  *
@@ -8,34 +8,58 @@
  * file that was distributed with this source code.
  */
 
-namespace Slave\Provider\EventDispatcher;
+namespace Fnayou\Slave\Provider\EventDispatcher;
 
 use Pimple\Container;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\Console\Event\ConsoleExceptionEvent;
+use Pimple\ServiceProviderInterface;
 use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\Console\Event\ConsoleErrorEvent;
+use Symfony\Component\Console\Exception\CommandNotFoundException;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 /**
  * Class EventDispatcherProvider.
  */
-class EventDispatcherProvider implements \Pimple\ServiceProviderInterface
+class EventDispatcherProvider implements ServiceProviderInterface
 {
     /**
      * @param Container $container
      */
     public function register(Container $container)
     {
-        $container['dispatcher'] = function () {
+        $container['dispatcher'] = function() {
             $dispatcher = new EventDispatcher();
 
-            // add listener to handle exception
-            $dispatcher->addListener(ConsoleEvents::EXCEPTION, function (ConsoleExceptionEvent $event) {
-                $output = $event->getOutput();
+            $dispatcher->addListener(ConsoleEvents::ERROR, function (ConsoleErrorEvent $event) {
+                $style = new SymfonyStyle($event->getInput(), $event->getOutput());
                 $command = $event->getCommand();
+                $error = $event->getError();
 
-                $output->writeln(sprintf(
-                    '<error>Exception :</error> Oops, exception thrown while running command : <info>%s</info>',
+                if ($error instanceof CommandNotFoundException) {
+                    throw $error;
+                }
+
+                $style->error(\sprintf(
+                    'Exception : Exception thrown while running command : %s',
                     $command->getName()
+                ));
+
+                $style->error(\sprintf(
+                    '%s : %s',
+                    $error->getCode(),
+                    $error->getMessage()
+                ));
+
+                $style->error(\sprintf(
+                    'File %s line %s',
+                    $error->getFile(),
+                    $error->getLine()
+                ));
+
+                $style->error(\sprintf(
+                    '%s',
+                    $error->getTraceAsString()
                 ));
             });
 
